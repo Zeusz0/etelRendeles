0a2a9d4b055d72852c2bb7b801527c1564165924
Kiri Attila István <h983606@stud.u-szeged.hu>
2021-11-05 11:30:17 +0100

    Closes #13

--- etelrendeles/comps/Navbar.js
+++ etelrendeles/comps/Navbar.js
@@ -8,17 +8,17 @@ const Navbar = () => {
         <Image src="/logo.png" width={128} height={80}/>
       </div>
       <Link href="/">
-        <a>Home</a>
+        <a>Kezdőlap</a>
       </Link>
       <Link href="/about">
-        <a>About </a>
+        <a>Rólunk</a>
       </Link>
       {/*Regiszrációs és Bejelentkező !!!gombok hozzáadása!!!*/}
       <Link href="/forms/Registration">
-        <a>Registration</a> 
+        <a>Regisztráció</a> 
       </Link>
       <Link href="/forms/Login">
-        <a>Login</a> 
+        <a>Bejelentkezés</a> 
       </Link>
     </nav>
   );
