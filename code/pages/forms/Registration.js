6b8e5cae874fbca6173c0fc89d71c7fcc44af74f
Zeusz0 <2000zeno@gmail.com>
2021-11-13 11:52:21 +0100

    prisma update

--- code/pages/forms/Registration.js
+++ code/pages/forms/Registration.js
@@ -6,6 +6,8 @@ const prisma = new PrismaClient();
 
 const Registration = ({data}) => {
 
+  const [formData, setFormData] = useState({})
+
   return (
     <>
       <Head>
@@ -22,6 +24,14 @@ const Registration = ({data}) => {
             <li key="item.id">{item.vezetek_nev}</li>
           ))}
         </ul>
+            
+        {/*<form onSubmit={saveMovie}>
+                    <input type="text" placeholder="Title" name="title" onChange={e => setFormData({ ...formData, title: e.target.value })}/>
+                    <input type="text" placeholder="Year" name="year" onChange={e => setFormData({ ...formData, year: +e.target.value })} />
+                    <textarea name="description" id="" cols="30" rows="10" placeholder="Description" onChange={e => setFormData({ ...formData, description: e.target.value })} />
+                    <input type="text" placeholder="Slug" name="slug" onChange={e => setFormData({ ...formData, slug: e.target.value })} />
+                    <button type="submit">Add movie</button>
+          </form> */}
       </div>
     </>
   )
