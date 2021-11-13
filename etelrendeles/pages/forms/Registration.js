fe1d6f20a46b385c4b1443b4458ec2f83a4cfede
Zeusz0 <2000zeno@gmail.com>
2021-11-13 16:42:20 +0100

    Closes #35

---
+++ etelrendeles/pages/forms/Registration.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Registration = () => {
+  return (
+    <>
+      <Head>
+        <title>Ételrendelés | Regisztráció</title>
+        <meta name="keywords" content="Etelrendeles, Regisztracio" />
+      </Head>
+      <div>
+        <h1>Regisztráció</h1>
+      </div>
+    </>
+  )
+}
+
+export default Registration;
