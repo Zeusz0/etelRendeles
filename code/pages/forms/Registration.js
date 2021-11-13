7faa92f1e1bf5b6211ea168073744f589952e8e6
Halmai Daniel <h983118@stud.u-szeged.hu>
2021-11-13 19:15:34 +0100

    regisztráció működik, de van egy tervezésbeli hibánk.. - close #30

--- code/pages/forms/Registration.js
+++ code/pages/forms/Registration.js
@@ -1,49 +1,77 @@
 import Head from "next/head";
-import { useState } from "react";
-import { PrismaClient } from "@prisma/client";
+import {useState} from "react";
+import {PrismaClient} from "@prisma/client";
+
 
 const prisma = new PrismaClient();
 
-const Registration = ({ data }) => {
-  const [formData, setFormData] = useState({});
 
-  return (
-    <>
-      <Head>
-        <title>Ételrendelés | Regisztráció</title>
-        <meta name="keywords" content="Etelrendeles, Regisztracio" />
-      </Head>
-      <div>
-        <h1>Regisztráció</h1>
-      </div>
-      <div>
-        <h1>Regisztrált felhasználóink: </h1>
-        <ul>
-          {data.map((item) => (
-            <li key="item.id">{item.nev}</li>
-          ))}
-        </ul>
+const Registration = ({data}) => {
+    const [formData, setFormData] = useState({});
 
-        {/*<form onSubmit={saveMovie}>
-                    <input type="text" placeholder="Title" name="title" onChange={e => setFormData({ ...formData, title: e.target.value })}/>
-                    <input type="text" placeholder="Year" name="year" onChange={e => setFormData({ ...formData, year: +e.target.value })} />
-                    <textarea name="description" id="" cols="30" rows="10" placeholder="Description" onChange={e => setFormData({ ...formData, description: e.target.value })} />
-                    <input type="text" placeholder="Slug" name="slug" onChange={e => setFormData({ ...formData, slug: e.target.value })} />
-                    <button type="submit">Add movie</button>
-          </form> */}
-      </div>
-    </>
-  );
+
+    async function registration(e) {
+        e.preventDefault()
+        const response = await fetch('/api/felhasznalok', {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+        console.log(formData);
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Ételrendelés | Regisztráció</title>
+                <meta name="keywords" content="Etelrendeles, Regisztracio"/>
+            </Head>
+            <div>
+                <h1>Regisztráció</h1>
+            </div>
+            <div>
+                <form onSubmit={registration}>
+                    <input type="text" placeholder="Név" name="nev"
+                           onChange={e => setFormData({...formData, nev: e.target.value})}/>
+                    <input type="email" placeholder="E-mail cím" name="email"
+                           onChange={e => setFormData({...formData, email: e.target.value})}/>
+
+                    {/*TODO kellene jelszó az adatbázisba F*/}
+                    {/*<input type="jelszo" placeholder="Jelszó" name="jelszo"*/}
+                    {/*       onChange={e => setFormData({...formData, jelszo: e.target.value})}/>*/}
+                    {/*<input type="jelszo_ujra" placeholder="Jelszó mégegyszer" name="jelszo_ujra"*/}
+                    {/*       onChange={e => setFormData({...formData, jelszo_ujra: e.target.value})}/>*/}
+
+                    <input type="tel" placeholder="Telefonszám" name="telefonszam"
+                           onChange={e => setFormData({...formData, telefonszam: e.target.value})}/>
+                    <textarea name="cim" cols="30" rows="3" placeholder="Szállítási cím"
+                              onChange={e => setFormData({...formData, cim: e.target.value})}/>
+                    <textarea name="szmlazasicim" cols="30" rows="3" placeholder="Számlázási cím"
+                              onChange={e => setFormData({...formData, szamlazasi_cim: e.target.value})}/>
+                    <p>Miként regisztrál?</p>
+                    <input type="radio" name="partner" value="false"
+                           onChange={e => setFormData({...formData, partner: false})}/>
+                    <label htmlFor="html">Felhasználó</label>
+                    <input type="radio" name="partner" value="true"
+                           onChange={e => setFormData({...formData, partner: true})}/>
+                    <label htmlFor="html">Partner</label>
+
+                    <button type="submit">Regisztráció</button>
+                    <button type="submit">Mégse</button>
+                </form>
+            </div>
+        </>
+    )
+        ;
 };
 
 export default Registration;
 
 export async function getServerSideProps() {
-  const users = await prisma.aru.findMany();
+    const users = await prisma.felhasznalok.findMany();
 
-  return {
-    props: {
-      data: users,
-    },
-  };
+    return {
+        props: {
+            data: users,
+        },
+    };
 }
