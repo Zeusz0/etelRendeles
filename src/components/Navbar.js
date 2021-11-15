3976863693b71b1e0ba82d0026d55abe05f55c75
Balázs Csete <csetebalu@gmail.com>
2021-11-15 23:37:30 +0100

    auth example (ugly css to encurage the usage of a component library)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 const Navbar = () => {
+  const { data: sessionData } = useSession();
+  const user = sessionData?.user;
+
   return (
-    <nav>
-      <div className="logo"></div>
-      <Link href="/">
-        <a>Kezdőlap</a>
+    <nav style={{ justifyContent: "space-between" }}>
+      <Link href="/" passHref>
+        <a>
+          <Image src="/logo.png" alt="Logo" width={50} height={50} />
+        </a>
       </Link>
-      <Link href="/about">
-        <a>Rólunk</a>
-      </Link>
-      <Link href="/forms/Registration">
-        <a>Regisztráció</a>
-      </Link>
-      <Link href="/forms/Login">
-        <a>Bejelentkezés</a>
-      </Link>
-      <Link href="/partners/partners">
-        <a>Partner éttermeink</a>
-      </Link>
+      <div>
+        <Link href="/">
+          <a>Kezdőlap</a>
+        </Link>
+        <Link href="/about">
+          <a>Rólunk</a>
+        </Link>
+        <Link href="/partners/partners">
+          <a>Partner éttermeink</a>
+        </Link>
+        {user ? (
+          <>
+            <p>Loggend in as {user.name}</p>
+            <button onClick={signOut}>Kijelentkezés</button>
+          </>
+        ) : (
+          <button onClick={signIn}>Bejelentkezés</button>
+        )}
+      </div>
     </nav>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
-import Head from "next/head"
+import Head from "next/head";
+import prisma from "../prisma";
 
-const About = () => {
+const About = ({ numOfUsers }) => {
   return (
     <>
       <Head>
@@ -10,22 +11,40 @@ const About = () => {
       <div>
         <h1>Rólunk</h1>
         <p>
-        A mai rohanó világban cégünk szeretné megkönnyíteni az éhes emberek számára, 
-        hogy éttermi partnereinknél könnyedén találjanak nagyszerű, sokszínű ételeket, 
-        amelyeket a futárszolgálatunk tagjai időben kiszállítanak. 
+          A mai rohanó világban cégünk szeretné megkönnyíteni az éhes emberek
+          számára, hogy éttermi partnereinknél könnyedén találjanak nagyszerű,
+          sokszínű ételeket, amelyeket a futárszolgálatunk tagjai időben
+          kiszállítanak.
         </p>
         <p>
-        Kapcsolatot építünk ki éttermek és kiskereskedők között, akik éleleket szeretnének készíteni és eladni,
-        a futárok között, akik keresni szeretnének ezen termékek kiszállításával, 
-        és az ügyfelek között, akik időt és energiát szeretnének spórolni, hogy az élet fontosabb dolgaira összpontosíthassanak.
-        Így városainkat is jobb hellyé tehetjük. Ezalatt pedig boldogabb embereket értünk. 
-        Boldogabb kisvállalkozásokat, amelyek több üzlettel rendelkeznek, és több embert tudnak foglalkoztatni, 
-        boldogabb futárokat, akiknek rugalmas módja van keresni, amikor úgy döntenek, 
-        boldogabb ügyfeleket, akik most könnyen hozzáférhetnek nagyszerű ételekhez. 
+          Kapcsolatot építünk ki éttermek és kiskereskedők között, akik éleleket
+          szeretnének készíteni és eladni, a futárok között, akik keresni
+          szeretnének ezen termékek kiszállításával, és az ügyfelek között, akik
+          időt és energiát szeretnének spórolni, hogy az élet fontosabb dolgaira
+          összpontosíthassanak. Így városainkat is jobb hellyé tehetjük. Ezalatt
+          pedig boldogabb embereket értünk. Boldogabb kisvállalkozásokat,
+          amelyek több üzlettel rendelkeznek, és több embert tudnak
+          foglalkoztatni, boldogabb futárokat, akiknek rugalmas módja van
+          keresni, amikor úgy döntenek, boldogabb ügyfeleket, akik most könnyen
+          hozzáférhetnek nagyszerű ételekhez.
         </p>
+        <p>Aktív felhasználóink száma: {numOfUsers}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+const getServerSideProps = async () => {
+  const numOfUsers = await prisma.user.count();
+
+  console.log(numOfUsers);
+
+  return {
+    props: {
+      numOfUsers,
+    },
+  };
+};
+
+export { getServerSideProps };
+export default About;
