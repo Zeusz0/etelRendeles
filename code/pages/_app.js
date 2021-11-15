b0f7ddb16a94d1404894594188ec70bf82db3f17
Zeusz0 <2000zeno@gmail.com>
2021-11-15 19:45:57 +0100

    auth taken out temporary

--- code/pages/_app.js
+++ code/pages/_app.js
@@ -1,14 +1,11 @@
 import Layout from "../comps/Layout";
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
--- code/pages/partners/partners.js
+++ code/pages/partners/partners.js
@@ -1,26 +1,10 @@
 import Head from "next/head";
 import { useState } from "react";
 import { PrismaClient } from "@prisma/client";
-import { useSession } from "next-auth/react";
 
 const prisma = new PrismaClient();
 
 const Partners = ({ data_user }) => {
-  const { data: session, status } = useSession();
-  if (status === "authenticated") {
-    return (
-      <div>
-        <h2>Üdv, {session.user.name}! Kitől rendeljünk?</h2>
-        {data_user.map((item) => (
-          <div key={item.id}>
-            <a>
-              <h3>{item.name}</h3>
-            </a>
-          </div>
-        ))}
-      </div>
-    );
-  }
   return (
     <div>
       <h1> Partnereink listája</h1>
