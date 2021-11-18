75eaf8b664d904aded61ad9bcdecdf39640f8ed9
Zeusz0 <2000zeno@gmail.com>
2021-11-18 16:02:21 +0100

    vercelProba auth-al

--- code/pages/index.js
+++ code/pages/index.js
@@ -1,8 +1,8 @@
-import Head from "next/head"
-import Image from "next/image"
-import Navbar from "../comps/Navbar"
-import Footer from "../comps/Footer"
-import styles from "../styles/Home.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../comps/Navbar";
+import Footer from "../comps/Footer";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,11 +14,12 @@ export default function Home() {
       <div>
         <h1 className={styles.title}>Kezdőlap</h1>
         <p>
-          Halihó!
-          Megéheztél? Vagy csak egy kis desszertre vágysz délután? Esetleg az esti bulihoz kéne egy hatalmas adag pizza?
-          Bármilyen napszakban állnak rendelkezésedre partnereink, válogass kedvedre és élvezd az ételt a saját otthonodban.
+          Halihó! Megéheztél? Vagy csak egy kis desszertre vágysz délután?
+          Esetleg az esti bulihoz kéne egy hatalmas adag pizza? Bármilyen
+          napszakban állnak rendelkezésedre partnereink, válogass kedvedre és
+          élvezd az ételt a saját otthonodban.
         </p>
       </div>
     </>
-  )
+  );
 }
--- code/pages/partners/partners.js
+++ code/pages/partners/partners.js
@@ -2,55 +2,65 @@ import Head from "next/head";
 import React, { useState } from "react";
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const prisma = new PrismaClient();
 
 const Partner = ({ data }) => {
+  const { data: session } = useSession();
   const [searchTerm, setSearchTerm] = useState("");
+  if (session) {
+    console.log(session);
+    console.log("be vagy jelentkezve");
+    return (
+      <>
+        <Head>
+          <title>Partner éttermeink</title>
+          <meta name="keywords" content="Etelrendeles, Partnerek" />
+        </Head>
+        <main>
+          <input
+            type="text"
+            placeholder="Keresés..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+          <ul>
+            {data
+              .filter((val) => {
+                if (setSearchTerm == "") {
+                  return val;
+                } else if (
+                  val.name
+                    .toLowerCase()
+                    .includes(searchTerm.toLocaleLowerCase())
+                ) {
+                  return val;
+                }
+              })
+              .map((val, key) => {
+                return (
+                  <div key={key}>
+                    <Link href={`../etelek/${val.id}`}>
+                      <a>{val.name}</a>
+                    </Link>
+                  </div>
+                );
+              })}
+          </ul>
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        </main>
+      </>
+    );
+  }
   return (
     <>
-      <Head>
-        <title>Partner éttermeink</title>
-        <meta name="keywords" content="Etelrendeles, Partnerek" />
-      </Head>
-      <main>
-        <input
-          type="text"
-          placeholder="Keresés..."
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        />
-        <ul>
-          {data
-            .filter((val) => {
-              if (setSearchTerm == "") {
-                return val;
-              } else if (
-                val.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-              ) {
-                return val;
-              }
-            })
-            .map((val, key) => {
-              return (
-                <div key={key}>
-                  <Link href={`../etelek/${val.id}`}>
-                    <a>{val.name}</a>
-                  </Link>
-                </div>
-              );
-            })}
-
-          {/*data.map((item) => (
-            <li key="item.id">
-              <Link href={`../etelek/${item.id}`}>
-                <a>{item.name}</a>
-              </Link>
-            </li>
-          ))*/}
-        </ul>
-      </main>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in with Github</button>
     </>
   );
 };
