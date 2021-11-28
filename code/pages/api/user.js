8eba4f3d02412a577dc7b4afa57656ab02a9f52e
Zeusz0 <2000zeno@gmail.com>
2021-11-28 17:48:41 +0100

    user data update

--- code/pages/api/user.js
+++ code/pages/api/user.js
@@ -2,17 +2,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async (req, res) => {
-    if (req.method === "POST") {
-        await prisma.User.create({
-            data: req.body,
-        });
-    }
-    if (req.method === "DELETE") {
-        await prisma.User.delete({
-            where: {
-                id: req.body.id,
-            },
-        });
-    }
-    res.status(200).send();
+  if (req.method === "POST") {
+    const { name, email, telefonszam, cim, szamlazasi_cim } = req.body;
+    await prisma.user.update({
+      where: {
+        id: req.body.id,
+      },
+      data: {
+        name: req.body.name,
+        email: req.body.email[0],
+        telefonszam: req.body.telefonszam[0],
+        cim: req.body.cim[0],
+        szamlazasi_cim: req.body.szamlazasi_cim[0],
+      },
+    });
+  }
+  if (req.method === "DELETE") {
+    await prisma.user.delete({
+      where: {
+        id: req.body.id,
+      },
+    });
+  }
+  res.status(200).send();
 };
